f79956b6c45ccbdd7724b73caacdb926f6d5c18a
TechDev-CEO <shoriful521399@gmail.com>
2022-01-25 10:27:23 +0600

    Route Defined done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview'
+import NotFound from './components/NotFound/NotFound';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Shop/>}/>
+        <Route exact path="/Shop" element={<Shop/>}/>
+        <Route exact path="/review" element={<OrderReview/>}></Route>
+        <Route exact path="/inventory" element={<Inventory/>}></Route>
+        <Route exact path="*" element ={<NotFound/>}>
+        </Route>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
