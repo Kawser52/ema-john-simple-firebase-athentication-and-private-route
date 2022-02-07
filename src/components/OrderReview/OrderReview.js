c1cd8a29f51b6e14c37178cc98104cf8d1750048
TechDev-CEO <shoriful521399@gmail.com>
2022-02-07 17:40:02 +0600

    product display just count done

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/usProduct';
+
 
 const OrderReview = () => {
+   const [product] = useProducts();
     return (
         <div>
+            <h2>{product.length}</h2>
             <h2>This is Order Review</h2>
         </div>
     );
